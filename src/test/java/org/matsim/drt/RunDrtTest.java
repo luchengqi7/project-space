5516666c95074ee0de8f6a53b5cec0bb4c3b4e3d
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2022-01-06 18:27:08 +0100

    Update test
    Add a dummy test to avoid build errors. Will replace it with a real test later

---
+++ src/test/java/org/matsim/drt/RunDrtTest.java
@@ -0,0 +1,15 @@
+package org.matsim.drt;
+
+import org.junit.Test;
+
+public class RunDrtTest {
+    @Test
+    public void testDrtDoorToDoor() {
+        //TODO here is a dummy test. Add a real test later
+        System.out.println("Starting dummy test");
+        double x = 1;
+        double y = 2;
+        double z = x + y;
+        assert x + y == z : "some thing is wrong!!!";
+    }
+}
