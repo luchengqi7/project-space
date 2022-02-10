27b1b5bfa57e79a4a5a090e6629341020c21471e
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2022-02-10 18:18:54 +0100

    Update OptimizationAgainstTrafficCount.java

--- src/main/java/org/matsim/playground/germanFreight/OptimizationAgainstTrafficCount.java
+++ src/main/java/org/matsim/playground/germanFreight/OptimizationAgainstTrafficCount.java
@@ -1,12 +1,12 @@
 package org.matsim.playground.germanFreight;
 
 import org.matsim.application.MATSimApplication;
-import org.matsim.application.prepare.freight.optimization.DetermineAverageTruckLoad;
 
 import java.net.MalformedURLException;
 
 public class OptimizationAgainstTrafficCount extends MATSimApplication {
     public static void main(String[] args) throws MalformedURLException {
-        DetermineAverageTruckLoad.main(args);
+        //DetermineAverageTruckLoad.main(args);  // TODO enable this line after the matsim-lib has been updated
+        System.out.println("Work in progress... Waiting for updates in matsim-lib");
     }
 }
