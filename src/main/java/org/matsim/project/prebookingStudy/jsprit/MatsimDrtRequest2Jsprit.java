f065dc7f3de19bf66d48045f73296c61abf4aff3
Hao Wu <56875899+haowuintub@users.noreply.github.com>
2022-03-17 11:09:51 +0100

    Squashed commit of the following:
    commit 766d824159d07d72bacd93d6af4614ffa90f7607
    Author: Hao Wu <56875899+haowuintub@users.noreply.github.com>
    Date:   Wed Mar 16 17:44:42 2022 +0100
    
        minor changes
    
    commit a0225b4da5b226402dfa9991520528e59d700f9c
    Author: Hao Wu <56875899+haowuintub@users.noreply.github.com>
    Date:   Wed Mar 16 09:33:25 2022 +0100
    
        minor changes
    
    commit 375ed4b515a9a9294ce97f28768c8752c4e2ca93
    Author: Hao Wu <56875899+haowuintub@users.noreply.github.com>
    Date:   Tue Mar 15 19:28:14 2022 +0100
    
        add test for RunJspritScenario
    
    commit c1eb1710eb030d6cabd2b592f4076364ad37f586
    Author: Hao Wu <56875899+haowuintub@users.noreply.github.com>
    Date:   Tue Mar 15 18:02:48 2022 +0100
    
        update gitignore
    
    commit bc534f61662bd7e25db8de9164e3b171d71bd126
    Author: Hao Wu <56875899+haowuintub@users.noreply.github.com>
    Date:   Mon Mar 14 22:14:51 2022 +0100
    
        add some information into Logger to debug

--- src/main/java/org/matsim/project/prebookingStudy/jsprit/MatsimDrtRequest2Jsprit.java
+++ src/main/java/org/matsim/project/prebookingStudy/jsprit/MatsimDrtRequest2Jsprit.java
@@ -218,7 +218,7 @@ public class MatsimDrtRequest2Jsprit {
                 if(enableNetworkBasedCosts) {
                     travelTime = router.calcLeastCostPath(network.getLinks().get(Id.createLinkId(pickupLocationId)).getToNode(), network.getLinks().get(Id.createLinkId(deliveryLocationId)).getToNode(), pickupTime, null, null).travelTime;
                 } else {
-                    travelTime = EuclideanDistanceCalculator.calculateDistance(Location.newInstance(pickupLocationX, pickupLocationY).getCoordinate(), Location.newInstance(deliveryLocationX, deliveryLocationY).getCoordinate()) / speed;
+                    travelTime = EuclideanDistanceCalculator.calculateDistance(Location.Builder.newInstance().setId(pickupLocationId).setCoordinate(Coordinate.newInstance(pickupLocationX, pickupLocationY)).build().getCoordinate(), Location.Builder.newInstance().setId(deliveryLocationId).setCoordinate(Coordinate.newInstance(deliveryLocationX, deliveryLocationY)).build().getCoordinate()) / speed;
                 }
                 double latestDeliveryTime;
                 if(isSchoolTraffic) {
@@ -232,7 +232,7 @@ public class MatsimDrtRequest2Jsprit {
 
                     //ToDo: change maximalWaitingtime to beta from config file
                     latestDeliveryTime = SchoolTrafficUtils.identifySchoolStartTime(destinationActivityType);
-                    double timeBetweenPickUpAndLatestDelivery = 1.5 * travelTime + 15 * 60;
+                    double timeBetweenPickUpAndLatestDelivery = maxTravelTimeAlpha * travelTime + maxTravelTimeBeta;
                     Shipment shipment = Shipment.Builder.newInstance(person.getId() + "#" + requestCount)
                             //.setName("myShipment")
                             .setPickupLocation(Location.Builder.newInstance().setId(pickupLocationId).setCoordinate(Coordinate.newInstance(pickupLocationX, pickupLocationY)).build())
