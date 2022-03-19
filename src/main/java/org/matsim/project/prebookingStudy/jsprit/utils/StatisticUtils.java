aeca908a3ae7910992a280d8169c6505667c7e08
Hao Wu <56875899+haowuintub@users.noreply.github.com>
2022-03-19 13:30:12 +0100

    add (vehicle) driven distance map

--- src/main/java/org/matsim/project/prebookingStudy/jsprit/utils/StatisticUtils.java
+++ src/main/java/org/matsim/project/prebookingStudy/jsprit/utils/StatisticUtils.java
@@ -105,8 +105,10 @@ public class StatisticUtils {
             Map<String, Double> realPickupDepartureTimeMap = new HashMap<>();
             Map<String, Double> routeTravelDistanceMap = new HashMap<>();
             Location lastStopLocation = null;
-            double lastStopDepartureTime = route.getStart().getEndTime();
             TourActivity prevAct = route.getStart();
+            double lastStopDepartureTime = prevAct.getEndTime();
+            Location vehicleLastLocation = prevAct.getLocation();
+            double drivenDistance = 0;
             for (TourActivity act : route.getActivities()) {
                 if((("pickupShipment").equals(act.getName()))|(("deliverShipment").equals(act.getName()))){
                     String jobId;
@@ -124,12 +126,14 @@ public class StatisticUtils {
                         } else {
                             lastLegDistance = transportCosts.getDistance(lastStopLocation, act.getLocation(), lastStopDepartureTime, null);
                         }
+                        drivenDistance += transportCosts.getDistance(vehicleLastLocation, act.getLocation(), lastStopDepartureTime, null);
                     } else {
                         if (lastStopLocation == null) {
                             lastLegDistance = 0.;
                         } else {
                             lastLegDistance = EuclideanDistanceCalculator.calculateDistance(lastStopLocation.getCoordinate(), act.getLocation().getCoordinate());
                         }
+                        drivenDistance += EuclideanDistanceCalculator.calculateDistance(vehicleLastLocation.getCoordinate(), act.getLocation().getCoordinate());
                     }
                     if (("pickupShipment").equals(act.getName())) {
                         //time-related:
@@ -155,6 +159,7 @@ public class StatisticUtils {
                         }
                         lastStopLocation = act.getLocation();
                         lastStopDepartureTime = act.getEndTime();
+                        vehicleLastLocation = act.getLocation();
                     } else if (("deliverShipment").equals(act.getName())) {
                         //time-related:
                         double deliveryTime = act.getArrTime();
@@ -184,10 +189,12 @@ public class StatisticUtils {
                         }
                         lastStopLocation = act.getLocation();
                         lastStopDepartureTime = act.getEndTime();
+                        vehicleLastLocation = act.getLocation();
                     }
                 }
             }
             TourActivity afterAct = route.getEnd();
+            drivenDistanceMap.put(route.getVehicle().getId(), drivenDistance);
         }
 
         if (enableNetworkBasedCosts) {
