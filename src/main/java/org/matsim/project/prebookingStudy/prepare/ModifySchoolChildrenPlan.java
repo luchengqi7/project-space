258096eb6b1c2cc2a0326cb349b764468905a25e
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2022-03-30 12:46:09 +0200

    Update ModifySchoolChildrenPlan.java

--- src/main/java/org/matsim/project/prebookingStudy/prepare/ModifySchoolChildrenPlan.java
+++ src/main/java/org/matsim/project/prebookingStudy/prepare/ModifySchoolChildrenPlan.java
@@ -1,6 +1,7 @@
 package org.matsim.project.prebookingStudy.prepare;
 
 import org.apache.log4j.Logger;
+import org.locationtech.jts.geom.Geometry;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
@@ -10,6 +11,7 @@ import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.Population;
 import org.matsim.api.core.v01.population.PopulationWriter;
 import org.matsim.application.MATSimAppCommand;
+import org.matsim.application.options.ShpOptions;
 import org.matsim.contrib.common.util.DistanceUtils;
 import org.matsim.contrib.drt.run.MultiModeDrtConfigGroup;
 import org.matsim.contrib.dvrp.trafficmonitoring.QSimFreeSpeedTravelTime;
@@ -23,6 +25,7 @@ import org.matsim.core.router.speedy.SpeedyALTFactory;
 import org.matsim.core.router.util.LeastCostPathCalculator;
 import org.matsim.core.router.util.TravelTime;
 import org.matsim.core.utils.geometry.CoordUtils;
+import org.matsim.core.utils.geometry.geotools.MGC;
 import picocli.CommandLine;
 
 import java.util.List;
@@ -36,6 +39,8 @@ import java.util.Random;
  */
 public class ModifySchoolChildrenPlan implements MATSimAppCommand {
     private static final Logger log = Logger.getLogger(ModifySchoolChildrenPlan.class);
+
+    private enum SchoolStartingTimeType {UNIFORM, TWO_STARTING_TIME}
 
     @CommandLine.Option(names = "--config", description = "path to config file", required = true)
     private String configPath;
@@ -55,6 +60,12 @@ public class ModifySchoolChildrenPlan implements MATSimAppCommand {
     @CommandLine.Option(names = "--output", description = "path to output plans file", required = true)
     private String outputPath;
 
+    @CommandLine.Option(names = "--school-starting-time", description = "path to output plans file", defaultValue = "UNIFORM")
+    private SchoolStartingTimeType schoolStartingTimeType;
+
+    @CommandLine.Mixin
+    private ShpOptions shp = new ShpOptions();
+
     private final Random random = new Random(1234);
 
     public static void main(String[] args) {
@@ -73,7 +84,16 @@ public class ModifySchoolChildrenPlan implements MATSimAppCommand {
 
         Network network = NetworkUtils.readNetwork(networkPath);
         Population plans = PopulationUtils.readPopulation(plansPath);
-        UniformSchoolStartingTimeIdentification schoolStartTimeCalculator = new UniformSchoolStartingTimeIdentification();
+
+        SchoolStartingTimeIdentifier schoolStartTimeCalculator;
+        if (schoolStartingTimeType == SchoolStartingTimeType.UNIFORM) {
+            schoolStartTimeCalculator = new UniformSchoolStartingTimeIdentification();
+        } else if (schoolStartingTimeType == SchoolStartingTimeType.TWO_STARTING_TIME) {
+            schoolStartTimeCalculator = new TwoSchoolStartingTimeIdentification(shp.getGeometry());
+        } else {
+            throw new RuntimeException("Unknown school starting time type!");
+        }
+
         TravelTime travelTime = new QSimFreeSpeedTravelTime(1.0);
         LeastCostPathCalculator router = new SpeedyALTFactory().createPathCalculator(network, new OnlyTimeDependentTravelDisutility(travelTime), travelTime);
 
@@ -111,13 +131,39 @@ public class ModifySchoolChildrenPlan implements MATSimAppCommand {
         return 0;
     }
 
+    interface SchoolStartingTimeIdentifier {
+        double getSchoolStartingTime(Activity schoolActivity);
+    }
+
     // Initial implementation: Uniform school starting time at 8:00.
-    // An interface for this is possible
-    static class UniformSchoolStartingTimeIdentification {
+    static class UniformSchoolStartingTimeIdentification implements SchoolStartingTimeIdentifier {
         private final static double schoolStartingTime = 28800;  //08:00
 
+        @Override
         public double getSchoolStartingTime(Activity schoolActivity) {
             return schoolStartingTime;
+        }
+    }
+
+    static class TwoSchoolStartingTimeIdentification implements SchoolStartingTimeIdentifier {
+        private final Geometry earlyStartArea;
+        private final static int schoolStartingTime1 = 27000; // 07:30
+        private final static int schoolStartingTime2 = 28800; // 08:00
+
+        TwoSchoolStartingTimeIdentification(Geometry earlyStartArea) {
+            this.earlyStartArea = earlyStartArea;
+        }
+
+        @Override
+        public double getSchoolStartingTime(Activity schoolActivity) {
+            if (MGC.coord2Point(schoolActivity.getCoord()).within(earlyStartArea)) {
+                String activityTypeWithStartingTime = schoolActivity.getType() + "_starting_at_" + schoolStartingTime1;
+                schoolActivity.setType(activityTypeWithStartingTime);
+                return schoolStartingTime1;
+            }
+            String activityTypeWithStartingTime = schoolActivity.getType() + "_starting_at_" + schoolStartingTime2;
+            schoolActivity.setType(activityTypeWithStartingTime);
+            return schoolStartingTime2;
         }
     }
 
