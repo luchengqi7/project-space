4217132bc17bd57ce34bef3207f23b31e5bf0b0b
Hao Wu <56875899+haowuintub@users.noreply.github.com>
2022-04-13 00:04:02 +0200

    add StatisticCollectorForIterationEndsListener

--- src/main/java/org/matsim/project/prebookingStudy/jsprit/listener/MyIterationEndsListener.java
+++ src/main/java/org/matsim/project/prebookingStudy/jsprit/listener/MyIterationEndsListener.java
@@ -3,12 +3,16 @@ package org.matsim.project.prebookingStudy.jsprit.listener;
 import com.graphhopper.jsprit.core.algorithm.listener.IterationEndsListener;
 import com.graphhopper.jsprit.core.problem.VehicleRoutingProblem;
 import com.graphhopper.jsprit.core.problem.solution.VehicleRoutingProblemSolution;
+import com.graphhopper.jsprit.core.util.Solutions;
 
 import java.util.Collection;
 
 public class MyIterationEndsListener implements IterationEndsListener {
+
     @Override
     public void informIterationEnds(int i, VehicleRoutingProblem problem, Collection<VehicleRoutingProblemSolution> solutions) {
-
+        int iterationNumber = i;
+        int fleetSize = Solutions.bestOf(solutions).getRoutes().size();
+        double costs = Solutions.bestOf(solutions).getCost();
     }
 }
