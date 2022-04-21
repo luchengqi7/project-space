5130ceb7aca2fb5427f7b03ff88b512c8b4d0a86
Hao Wu <56875899+haowuintub@users.noreply.github.com>
2022-04-21 17:49:39 +0200

    add best costs and best fleet size in the IterationEndsListener

--- src/main/java/org/matsim/project/prebookingStudy/jsprit/listener/MyIterationEndsListener.java
+++ src/main/java/org/matsim/project/prebookingStudy/jsprit/listener/MyIterationEndsListener.java
@@ -12,10 +12,27 @@ public class MyIterationEndsListener implements IterationEndsListener {
 
     @Override
     public void informIterationEnds(int i, VehicleRoutingProblem problem, Collection<VehicleRoutingProblemSolution> solutions) {
+
+        // save costs and fleet size from current iteration
         double costs = Solutions.bestOf(solutions).getCost();
         int fleetSize = Solutions.bestOf(solutions).getRoutes().size();
-
         StatisticCollectorForIterationEndsListener.getCostsMap().put(i, costs);
         StatisticCollectorForIterationEndsListener.getFleetSizeMap().put(i, fleetSize);
+        if (i == 1) {
+            StatisticCollectorForIterationEndsListener.setBestCosts(costs);
+            StatisticCollectorForIterationEndsListener.setBestFleetSize(fleetSize);
+        }
+
+        double bestCosts = StatisticCollectorForIterationEndsListener.getBestCosts();
+        int bestFleetSize = StatisticCollectorForIterationEndsListener.getBestFleetSize();
+        // save best costs and best fleet size
+        if (costs < bestCosts) {
+            bestCosts = costs;
+        }
+        if (fleetSize < bestFleetSize) {
+            bestFleetSize = fleetSize;
+        }
+        StatisticCollectorForIterationEndsListener.getBestCostsMap().put(i, bestCosts);
+        StatisticCollectorForIterationEndsListener.getBestFleetSizeMap().put(i, bestFleetSize);
     }
 }
