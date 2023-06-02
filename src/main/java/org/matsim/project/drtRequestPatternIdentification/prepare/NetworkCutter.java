8025ea80c415b5375013d3fb395d1aab58739164
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2023-06-02 14:47:48 +0200

    Create drt trip generation file
    Generating all possible drt trips for random selection

---
+++ src/main/java/org/matsim/project/drtRequestPatternIdentification/prepare/NetworkCutter.java
@@ -0,0 +1,63 @@
+package org.matsim.project.drtRequestPatternIdentification.prepare;
+
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.Point;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.application.MATSimAppCommand;
+import org.matsim.application.options.ShpOptions;
+import org.matsim.core.network.NetworkUtils;
+import org.matsim.core.network.algorithms.NetworkCleaner;
+import org.matsim.core.utils.geometry.geotools.MGC;
+import picocli.CommandLine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NetworkCutter implements MATSimAppCommand {
+    @CommandLine.Option(names = "--network", description = "path to network file", required = true)
+    private String networkPath;
+
+    @CommandLine.Option(names = "--output", description = "path to output network", required = true)
+    private String outputNetwork;
+
+    @CommandLine.Option(names = "--pt", description = "keep PT links", defaultValue = "false")
+    private boolean keepPT;
+
+    @CommandLine.Mixin
+    private ShpOptions shp = new ShpOptions();
+
+    public static void main(String[] args) {
+        new NetworkCutter().execute(args);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        Network network = NetworkUtils.readNetwork(networkPath);
+        Geometry areaToKeep = shp.getGeometry();
+
+        List<Link> linksToRemove = new ArrayList<>();
+        System.out.println("Total links" + network.getLinks().size());
+        int counter = 0;
+        for (Link link : network.getLinks().values()) {
+            Point from = MGC.coord2Point(link.getFromNode().getCoord());
+            Point to = MGC.coord2Point(link.getToNode().getCoord());
+            if (!from.within(areaToKeep) && !to.within(areaToKeep)) {
+                linksToRemove.add(link);
+            }
+            counter++;
+            if (counter % 1000 == 0) {
+                System.out.println("Processing: " + counter + " processed.");
+            }
+        }
+        for (Link link : linksToRemove) {
+            network.removeLink(link.getId());
+        }
+
+        NetworkCleaner cleaner = new NetworkCleaner();
+        cleaner.run(network);
+
+        NetworkUtils.writeNetwork(network, outputNetwork);
+        return 0;
+    }
+}
