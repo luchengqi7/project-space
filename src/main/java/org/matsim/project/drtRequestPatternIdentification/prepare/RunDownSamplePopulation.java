992d691600c6cc3d2c66b54ecb52f8c2307170c5
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2023-06-05 11:52:48 +0200

    Create RunDownSamplePopulation.java

---
+++ src/main/java/org/matsim/project/drtRequestPatternIdentification/prepare/RunDownSamplePopulation.java
@@ -0,0 +1,9 @@
+package org.matsim.project.drtRequestPatternIdentification.prepare;
+
+import org.matsim.application.prepare.population.DownSamplePopulation;
+
+public class RunDownSamplePopulation {
+    public static void main(String[] args) {
+        new DownSamplePopulation().execute(args);
+    }
+}
