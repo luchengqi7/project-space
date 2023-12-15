8840b2802ee95e4f75ca6820b860a13334c91a9e
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2023-12-15 11:59:22 +0100

    Update SimpleZoneGenerator.java

--- src/main/java/org/matsim/project/drt_zone_generation/SimpleZoneGenerator.java
+++ src/main/java/org/matsim/project/drt_zone_generation/SimpleZoneGenerator.java
@@ -1,20 +1,27 @@
 package org.matsim.project.drt_zone_generation;
 
+import com.google.common.base.Preconditions;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.network.NetworkWriter;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Population;
+import org.matsim.application.analysis.DefaultAnalysisMainModeIdentifier;
 import org.matsim.contrib.drt.analysis.zonal.DrtZonalSystem;
 import org.matsim.contrib.dvrp.router.TimeAsTravelDisutility;
 import org.matsim.contrib.dvrp.trafficmonitoring.QSimFreeSpeedTravelTime;
 import org.matsim.contrib.zone.skims.SparseMatrix;
 import org.matsim.contrib.zone.skims.TravelTimeMatrices;
 import org.matsim.core.network.NetworkUtils;
+import org.matsim.core.population.PopulationUtils;
+import org.matsim.core.router.MainModeIdentifier;
+import org.matsim.core.router.TripStructureUtils;
 import org.matsim.core.router.util.TravelDisutility;
 import org.matsim.core.router.util.TravelTime;
-import scala.Int;
 
 import java.util.*;
 
@@ -23,14 +30,15 @@ import java.util.*;
  * time threshold from the centroid point.
  */
 public class SimpleZoneGenerator {
-    // TODO 1. Include # of departures on the link when ranking (use input plans)
+    // (Done) 1. Include # of departures on the link when ranking (use input plans)
     // TODO 2. Add some disturbance in the ranking and generate zones multiple times and get the best one
     // TODO 3. Speed up (e.g. multi-threading?)
     private final Network network;
     private final double timeRadius;
 
     private final Map<Id<Link>, Set<Id<Link>>> reachableLinksMap = new HashMap<>();
-    private final Map<Id<Link>, Integer> numReachableLinksMap = new HashMap<>();
+    private final Map<Id<Link>, Double> departuresAndArrivalsScore = new HashMap<>();
+    private final Map<Id<Link>, Double> linksScoringMap = new HashMap<>();
     private final SparseMatrix freeSpeedTravelTimeSparseMatrix;
 
     private final Map<Id<Link>, List<Link>> zonalSystemData = new LinkedHashMap<>();
@@ -58,9 +66,40 @@ public class SimpleZoneGenerator {
 
     public static void main(String[] args) {
         Network inputNetwork = NetworkUtils.readNetwork(args[0]);
+        String visualisationNetworkPath = args[1];
+        Population inputPlans = args.length == 3 ? PopulationUtils.readPopulation(args[2]) : null;
+
         SimpleZoneGenerator zoneGenerator = new SimpleZoneGenerator(inputNetwork);
+        zoneGenerator.processPlans(inputPlans);
         zoneGenerator.analyzeNetwork();
-        zoneGenerator.calculateZonesOnNetwork(args[1]);
+        zoneGenerator.selectCentroids();
+        zoneGenerator.createZonesOnNetwork(visualisationNetworkPath);
+    }
+
+    public void processPlans(Population inputPlans) {
+        network.getLinks().keySet().forEach(linkId -> departuresAndArrivalsScore.put(linkId, 0.));
+        if (inputPlans == null) {
+            return;
+        }
+        MainModeIdentifier mainModeIdentifier = new DefaultAnalysisMainModeIdentifier();
+        for (Person person : inputPlans.getPersons().values()) {
+            for (TripStructureUtils.Trip trip : TripStructureUtils.getTrips(person.getSelectedPlan())) {
+                if (mainModeIdentifier.identifyMainMode(trip.getTripElements()).equals(TransportMode.drt)) {
+                    Id<Link> fromLinkId = trip.getOriginActivity().getLinkId();
+                    if (fromLinkId == null) {
+                        fromLinkId = NetworkUtils.getNearestLink(network, trip.getOriginActivity().getCoord()).getId();
+                    }
+                    Id<Link> toLinkId = trip.getDestinationActivity().getLinkId();
+                    if (toLinkId == null) {
+                        toLinkId = NetworkUtils.getNearestLink(network, trip.getDestinationActivity().getCoord()).getId();
+                    }
+                    double fromLinkNewValue = departuresAndArrivalsScore.get(fromLinkId) + 1;
+                    departuresAndArrivalsScore.put(fromLinkId, fromLinkNewValue);
+                    double toLinkNewValue = departuresAndArrivalsScore.get(toLinkId) + 0.5;
+                    departuresAndArrivalsScore.put(toLinkId, toLinkNewValue);
+                }
+            }
+        }
     }
 
 
@@ -70,7 +109,7 @@ public class SimpleZoneGenerator {
         int pct = 0;
         int tenPct = numLinks / 10;
 
-        // Explore reachable links and forbidden link
+        // Explore reachable links
         log.info("Begin analyzing network " + pct + "% completed");
         network.getLinks().keySet().forEach(linkId -> reachableLinksMap.put(linkId, new HashSet<>()));
         for (Link fromLink : network.getLinks().values()) {
@@ -118,18 +157,16 @@ public class SimpleZoneGenerator {
             }
         }
 
-        reachableLinksMap.forEach((linkId, ids) -> numReachableLinksMap.put(linkId, ids.size()));
+        reachableLinksMap.forEach((linkId, ids) -> linksScoringMap.put(linkId, ids.size() + departuresAndArrivalsScore.get(linkId)));
     }
 
-    public void calculateZonesOnNetwork(String outputNetwork) {
-        // Selecting centroid by sorting the links based on num of reachable links from there
+    public void selectCentroids() {
         log.info("Begin choosing centroid of zones");
         List<Id<Link>> rankedPotentialCentroids = new ArrayList<>();
-        numReachableLinksMap.entrySet().
+        linksScoringMap.entrySet().
                 stream().
                 sorted(Map.Entry.comparingByValue(Collections.reverseOrder())).
                 forEach(entry -> rankedPotentialCentroids.add(entry.getKey()));
-
         while (!rankedPotentialCentroids.isEmpty()) {
             Id<Link> chosenLinkId = rankedPotentialCentroids.get(0);
             zonalSystemData.put(chosenLinkId, new ArrayList<>());
@@ -138,14 +175,17 @@ public class SimpleZoneGenerator {
             rankedPotentialCentroids.removeAll(reachableLinksMap.get(chosenLinkId));
         }
         log.info("Centroids generation completed. There are " + zonalSystemData.size() + " centroids (i.e., zones)");
+    }
 
+    public void createZonesOnNetwork(String outputNetwork) {
         // Add links to the zone
         log.info("Assigning links into zones (based on closest centroid)");
+        // Record the neighbour for coloring (visualisation)
         Map<String, Set<String>> zoneNeighborsMap = new HashMap<>();
         zonalSystemData.keySet().forEach(linkId -> zoneNeighborsMap.put(linkId.toString(), new HashSet<>()));
 
         for (Id<Link> linkId : network.getLinks().keySet()) {
-            Set<String> zones = new HashSet<>();
+            Set<String> relevantZones = new HashSet<>();
             double minDistance = Double.POSITIVE_INFINITY;
             Id<Link> closestCentralLinkId = null;
             for (Id<Link> centroidLinkId : zonalSystemData.keySet()) {
@@ -163,23 +203,23 @@ public class SimpleZoneGenerator {
                         minDistance = distance;
                         closestCentralLinkId = centroidLinkId;
                     }
-                    zones.add(centroidLinkId.toString());
+                    relevantZones.add(centroidLinkId.toString());
                 }
             }
             zonalSystemData.get(closestCentralLinkId).add(network.getLinks().get(linkId));
-            if (zones.size() > 1) {
-                // neighboring zones are detected
-                for (String zoneId : zones) {
-                    zoneNeighborsMap.get(zoneId).addAll(zones);
+            if (relevantZones.size() > 1) {
+                // if a link is relevant to (i.e., reachable from) more than one zone, then those zones are neighbours
+                for (String zoneId : relevantZones) {
+                    zoneNeighborsMap.get(zoneId).addAll(relevantZones);
                 }
             }
         }
 
         // Add attribute to the link for visualisation
-        log.info("Marking the links in the zones");
+        log.info("Marking links in each zone");
         // Determine the color of each zone (for visualisation)
         Map<String, Integer> coloringMap = new HashMap<>();
-        network.getLinks().keySet().forEach(linkId -> coloringMap.put(linkId.toString(), 0));
+        zoneNeighborsMap.keySet().forEach(zoneId -> coloringMap.put(zoneId, 0));
         for (String zoneId : zoneNeighborsMap.keySet()) {
             Set<Integer> usedColor = new HashSet<>();
             for (String neighboringZone : zoneNeighborsMap.get(zoneId)) {
@@ -214,9 +254,7 @@ public class SimpleZoneGenerator {
             }
         }
         new NetworkWriter(network).write(outputNetwork);
-
     }
-
 
     //TODO complete this after testing
     public DrtZonalSystem writeDrtZonalSystems() {
